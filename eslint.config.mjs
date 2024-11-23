af4185946ed8e1a83b7126497323f1e0879f5ac9
Daniel Cruz <danielcunha54@gmail.com>
2024-11-23 08:19:51 -0300

    chore: create react project

---
+++ eslint.config.mjs
@@ -0,0 +1,68 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  ...tseslint.config(
+    { ignores: ['dist'] },
+    {
+      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      files: ['**/*.{ts,tsx,mjs}'],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        'simple-import-sort': simpleImportSort,
+        'unused-imports': unusedImports,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'simple-import-sort/imports': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_',
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ),
+  prettier,
+  {
+    rules: {
+      'no-useless-constructor': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: true,
+          endOfLine: 'auto',
+        },
+        {
+          fileInfoOptions: {
+            withNodeModules: true,
+          },
+        },
+      ],
+    },
+  },
+];
