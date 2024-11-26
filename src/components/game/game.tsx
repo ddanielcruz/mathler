f320240bf9eb0e87f6464f7e126e282b6896fdbd
Daniel Cruz <danielcunha54@gmail.com>
2024-11-26 11:38:35 -0300

    feat: show expected equation result

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,9 +1,18 @@
+import { useGame } from '@/contexts/game';
+
 import { Guesses } from './guesses';
 import { Keyboard } from './keyboard';
 
 export function Game() {
+  const { equationResult } = useGame();
+
   return (
     <div className="flex flex-col items-center gap-8">
+      <h2 className="text-center text-xl font-medium text-white">
+        Find the hidden calculation that{' '}
+        <div className="inline-flex rounded-lg bg-amber-500 px-1">equals {equationResult}</div>
+      </h2>
+
       <Guesses />
       <Keyboard />
     </div>
