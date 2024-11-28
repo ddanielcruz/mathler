2e3e77a0e768991c32305b5be017d8709753d50f
Daniel Cruz <danielcunha54@gmail.com>
2024-11-28 07:45:30 -0300

    feat(wip): add how to play dialog

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { Game } from './components/game';
+import { HowToPlay } from './components/how-to-play';
 import { GameProvider } from './contexts/game';
 
 export function App() {
   return (
     <GameProvider>
+      <HowToPlay />
       <main className="flex min-h-svh flex-col items-center justify-center px-4">
         <Game />
       </main>
---
+++ src/components/how-to-play.tsx
@@ -0,0 +1,49 @@
+import { Guess } from './game/guess';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+
+export function HowToPlay() {
+  return (
+    <Dialog>
+      <DialogTrigger className="text-white">How to play?</DialogTrigger>
+      <DialogContent className="text-gray-700 sm:w-[calc(3.5rem*6+0.375rem*5)+3rem] sm:max-w-min">
+        <DialogHeader>
+          <DialogTitle className="text-blue-700">How to play?</DialogTitle>
+        </DialogHeader>
+
+        <p>Try to find the hidden calculation in 6 guesses!</p>
+        <p>
+          After each guess, the color of the tiles will change to show how close you are to the
+          solution.
+        </p>
+
+        <Guess
+          guess={{
+            guess: [
+              { key: '5', state: 'present' },
+              { key: '0', state: 'absent' },
+              { key: '/', state: 'correct' },
+              { key: '5', state: 'present' },
+              { key: '-', state: 'absent' },
+              { key: '2', state: 'absent' },
+            ],
+            state: 'submitted',
+          }}
+        />
+
+        <ul className="list-disc space-y-0.5 px-4">
+          <li>Green are in the correct place.</li>
+          <li>Yellow are in the equation, but in a different place.</li>
+          <li>Gray are not in the equation.</li>
+        </ul>
+
+        <h3 className="text-xl font-medium text-blue-700">Additional rules</h3>
+        <ul className="list-disc space-y-0.5 px-4">
+          <li>Numbers and operators can appear multiple times.</li>
+          <li>Calculate / or * before - or + (order of operations).</li>
+          <li>Commutative solutions are accepted, for example 20+7+3 and 3+7+20.</li>
+          <li>Commutative solutions will be automatically rearranged to the exact solution.</li>
+        </ul>
+      </DialogContent>
+    </Dialog>
+  );
+}
